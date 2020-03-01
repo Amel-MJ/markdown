3c240015b0c90e3b31b8dc3e9a88eaa16ef319ed
Amel-MJ <web.amel.m@gmail.com>
2020-03-01 16:33:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+//const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+class App extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handelCancel = () => {
+    this.setState({
+      value: ''
+    });
+  }
+   handleEditorChange=({html, text})=> {    
+    console.log('handleEditorChange', html, text)
+  }
+  render() {
+    return (
+      <form>
+        {/* <MdEditor
+      value=""
+      renderHTML={(text) => mdParser.render(text)}
+      onChange={this.handleEditorChange}
+       />*/}
+        <div >
+          <div className='top'>
+          <h2>Content</h2>
+          <span>
+        <button className='button' value='cancel' onClick={this.handelCancel}>Cancel</button>
+        <button className='button ' value='submit' onClick={this.handelSubmit}>Save</button>
+        </span>
+         
+          </div>
+          
+          <textarea className="textarea"
+
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+          />
+        </div>
+        <div  className="checkbox">
+          <h2>Type</h2>
+
+          <input type="checkbox" id="text" name="text" value="text" />
+          <label for="text"> Text</label><br />
+          <input type="checkbox" id="img" name="img" value="img" />
+          <label for="img"> Image</label><br />
+          <input type="checkbox" id="note" name="note" value="note" />
+          <label for="note"> Note</label><br />
+
+
+        </div>
+        <div >
+          <h2>Note</h2>
+          <textarea className="textareaNote"/>
+
+        </div>
+      </form>
+    )
+  }
 }
 
 export default App;
